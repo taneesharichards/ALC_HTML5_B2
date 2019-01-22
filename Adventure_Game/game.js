31bc442f9e4b2c16edd4d29031a4b15c17f1152e
Unknown <taneesha.r166@stu.nebo.edu>
2019-01-22 10:30:11 -0700

    Started prompt examples

---
+++ Adventure_Game/game.js
@@ -0,0 +1,13 @@
+// Single line comment
+/*
+Multi-line comment
+*/
+
+//alert("Warning! Will Robinson...Warning!");
+//
+//confirm("Do you like pokemon?");
+//
+//prompt("What type of pokemon do you like?");
+
+var playerName = prompt("What is your name?");
+document.write(playerName);
